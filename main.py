78cd6da6bf36956499a2a5e4d46851593bfddcca
Danil Zhiharev <zhiharev.danil00@gmail.com>
2021-10-20 16:40:41 +0300

    asd

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_weather(city, open_weather_token):
         "Rain": "Дождь \U00002614",
         "Drizzle": "Дождь \U00002614",
         "Thunderstorm": "Гроза \U000026A1",
-        "Snow": "Снег \U0001F328",
+        "Snow": "Снегasd \U0001F328",
         "Mist": "Туман \U0001F32B"
     }
 
